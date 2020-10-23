acd18effe591e5d292d3059f5e21731b66bbc4b6
Gabriel Mathias <KatsukeKaoru@gmail.com>
2020-10-23 14:24:23 -0300

    Arquivo index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req,res){
+
+     res.send("Olá, Nobre Gabriel Mathias")
+
+
+})
+
+
+app.listen(3000, function(){
+
+     console.log("conexão inicializada na porta 3000")
+
+
+})
